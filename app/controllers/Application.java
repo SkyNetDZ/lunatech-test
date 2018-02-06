f6ca08f2cb542f937392a1c6051fea2d167978ec
SkyNetDZ <hedhadia.faycel@outlook.fr>
2018-02-06 17:59:58 +0100

    Application controller

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,8 @@ package controllers;
 import io.ebean.Ebean;
 import models.Country;
 import play.*;
+import play.data.Form;
+import play.data.FormFactory;
 import play.mvc.*;
 
 import views.html.*;
@@ -13,14 +15,18 @@ import java.util.List;
 public class Application extends Controller {
 
     private List<Country> countries;
+    Form<Country> countryForm ;
 
-    @Inject
-    public Application() {
-        this.countries = Ebean.find(Country.class).fetch("airports").where().eq("code","US").findList();
-    }
+
+//    @Inject
+//    public Application() {
+//        this.countries = Ebean.find(Country.class).fetch("airports").where().eq("code","US").findList();
+//    }
+
+
 
     public Result index() {
-        return ok(index.render(this.countries));
+        return ok(index.render(countryForm));
     }
 
 }
